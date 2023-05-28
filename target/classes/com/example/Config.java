540d5346d2e7be02249e9562c059afad89c65a61
garima kumar <garimasgv01991@gmail.com>
2023-05-28 03:14:12 -0700

    first commit

---
+++ target/classes/com/example/Config.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+
+
+@Configuration
+@ComponentScan(basePackages = "com.example")
+public class Config {
+
+}
